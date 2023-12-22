b98ec34cea68f10a3a32c55e8a7e44d239a3a9e7
Konrad Stefanski <stefak111_aehit@students.vizja.pl>
2023-12-22 19:23:26 +0100

    nowe api:users (by_username),  posts (post, get, delete)

--- app/main.py
+++ app/main.py
@@ -26,12 +26,13 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.post("/users", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
-    db_user = models.User(**user.dict())
+    db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
 
@@ -42,3 +43,34 @@ async def read_user(user_id: int, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+@app.get("/users/by_username/{username}", status_code=status.HTTP_200_OK)
+async def get_user_by_username(username: str, db: db_dependency):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user_id": user.id, "username": user.username}
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
+    db_post = models.Post(**post.model_dump())
+    db.add(db_post)
+    db.commit()
+
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def read_post(post_id: int, db: db_dependency):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail='Post was not found')
+    return post
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail='Post was not found')
+    db.delete(db_post)
